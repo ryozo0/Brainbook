bde97693365e1df11bf57c5687dfa3064e996a51
岡田亮介 <tobiuokun@gmail.com>
2017-07-31 18:19:42 +0900

    firstcommit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,47 @@
+class PostsController < ApplicationController
+before_action :set_post, only: [:edit, :update, :destroy]
+
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.create(posts_params)
+    if @post.save
+      redirect_to posts_path, notice: "投稿しました！"
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+
+  end
+
+  def update
+    if @post.update(posts_params)
+      redirect_to posts_path, notice: "編集しました！"
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to posts_path, notice: "削減しました！"
+  end
+
+  private
+    def posts_params
+      params.require(:post).permit(:title, :content)
+    end
+
+    def set_post
+      @post = Post.find(params[:id])
+    end
+
+end
